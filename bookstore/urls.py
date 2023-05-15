c63d51aa4bd7e337e0ed5dddf5727da41ccb6145
Bruno Saldanha <brunosaldanhamarinho@gmail.com>
2023-05-15 01:39:56 -0300

    teste projeto

---
+++ bookstore/urls.py
@@ -0,0 +1,32 @@
+"""bookstore URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import to include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+import debug_toolbar
+from django.contrib import admin
+from django.urls import path, re_path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls import include
+from rest_framework.authtoken.views import obtain_auth_token
+
+if settings.DEBUG:
+    import debug_toolbar
+urlpatterns = [
+    path("__debug__/", include(debug_toolbar.urls)),
+    path("admin/", admin.site.urls),
+    re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
+    re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
+    path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
+]
