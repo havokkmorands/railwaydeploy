c14732d8a630d6a502133920fbcca21a6bd7c715
Bruno Saldanha <brunosaldanhamarinho@gmail.com>
2023-05-15 03:29:52 -0300

    Update settings.py

--- controle/controle/settings.py
+++ controle/controle/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#xs&bk@f6m)h7p!xdtoxq89^5+n8)t7&vt7sw^7wms7w^n%(#%'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
